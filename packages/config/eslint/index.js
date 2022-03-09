990f83e5da1530d6d5aca0fd934bf9044314c947
Stef Winterswijk <stef_winterswijk@hotmail.com>
2022-03-09 16:02:50 +0100

    Update eslint config

--- packages/config/eslint/index.js
+++ packages/config/eslint/index.js
@@ -1,6 +1,9 @@
 module.exports = {
     root: true,
     parser: '@babel/eslint-parser',
+    parserOptions: {
+        requireConfigFile: false,
+    },
     env: {
         browser: true,
         commonjs: true,

dd7e70525c4398e0b013e8434b8f31c667fc636b
Stef Winterswijk <stef_winterswijk@hotmail.com>
2022-03-23 16:01:05 +0100

    Update FieldRte

--- packages/components/src/Form/FieldRte/FieldRte.tsx
+++ packages/components/src/Form/FieldRte/FieldRte.tsx
@@ -6,50 +6,6 @@ import ReactQuill, { ReactQuillProps } from 'react-quill'
 import { FieldLabel } from '../FieldLabel'
 import { quillDecodeIndent } from '../../utils/quillFixIndent'
 
-const DEFAULT_MODULES = {
-    toolbar: [
-        ['bold', 'italic', 'underline'],
-        [{ list: 'bullet' }, { list: 'ordered' }],
-    ],
-    keyboard: {
-        bindings: {
-            indent: {
-                key: 9,
-                format: ['blockquote', 'indent', 'list'],
-                handler: function (this: any, range: { index: number }) {
-                    // We want to disable the indentation if:
-                    // - (1) The current line is the first line and the indent level is 0 (not indented)
-                    // - (2) The current line is a list and the previous line is not a list
-                    // - (3) The current line is a list and the previous line too, but the previous lines indentation level is already one level lower
-
-                    const currentLineFormats = this.quill.getFormat(range.index)
-                    const previousLineFormats = this.quill.getFormat(
-                        range.index - 1
-                    )
-                    const currentLineIsAList = !!currentLineFormats.list
-                    const previousLineIsAList = !!previousLineFormats.list
-                    const currentLineIndent = currentLineFormats.indent || 0
-                    const previousLineIndent = previousLineFormats.indent || 0
-
-                    if (
-                        (range.index === 0 && currentLineIndent === 0) ||
-                        (currentLineIsAList && !previousLineIsAList) ||
-                        (currentLineIsAList &&
-                            previousLineIsAList &&
-                            previousLineIndent === currentLineIndent - 1)
-                    ) {
-                        return
-                    }
-
-                    return this.quill.format('indent', '+1', 'user')
-                },
-            },
-        },
-    },
-}
-
-const DEFAULT_FORMATS = ['bold', 'italic', 'underline', 'list', 'indent']
-
 export interface FieldRteProps extends ReactQuillProps {
     name: string
     label?: string
@@ -99,7 +55,7 @@ export const FieldRte = ({
                 <ReactQuill
                     theme="snow"
                     formats={formats}
-                    modules={modules}
+                    modules={{ ...modules, ...keyBoardBindings }}
                     className={classes}
                     onChange={handleChange}
                     readOnly={disabled}
@@ -108,4 +64,51 @@ export const FieldRte = ({
             </div>
         </>
     )
+}
+
+const DEFAULT_MODULES = {
+    toolbar: [
+        ['bold', 'italic', 'underline'],
+        [{ list: 'bullet' }, { list: 'ordered' }],
+    ],
+}
+
+const DEFAULT_FORMATS = ['bold', 'italic', 'underline', 'list', 'indent']
+
+const keyBoardBindings = {
+    keyboard: {
+        bindings: {
+            indent: {
+                key: 9,
+                format: ['blockquote', 'indent', 'list'],
+                handler: function (this: any, range: { index: number }) {
+                    // We want to disable the indentation if:
+                    // - (1) The current line is the first line and the indent level is 0 (not indented)
+                    // - (2) The current line is a list and the previous line is not a list
+                    // - (3) The current line is a list and the previous line too, but the previous lines indentation level is already one level lower
+
+                    const currentLineFormats = this.quill.getFormat(range.index)
+                    const previousLineFormats = this.quill.getFormat(
+                        range.index - 1
+                    )
+                    const currentLineIsAList = !!currentLineFormats.list
+                    const previousLineIsAList = !!previousLineFormats.list
+                    const currentLineIndent = currentLineFormats.indent || 0
+                    const previousLineIndent = previousLineFormats.indent || 0
+
+                    if (
+                        (range.index === 0 && currentLineIndent === 0) ||
+                        (currentLineIsAList && !previousLineIsAList) ||
+                        (currentLineIsAList &&
+                            previousLineIsAList &&
+                            previousLineIndent === currentLineIndent - 1)
+                    ) {
+                        return
+                    }
+
+                    return this.quill.format('indent', '+1', 'user')
+                },
+            },
+        },
+    },
 }

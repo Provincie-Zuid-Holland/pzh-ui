48a68fa2cd6631c234153039046b5d844ab3fac5
Stef Winterswijk <stef_winterswijk@hotmail.com>
2022-04-19 16:38:35 +0200

    Add Tooltip

---
+++ packages/components/src/Tooltip/Tooltip.stories.tsx
@@ -0,0 +1,19 @@
+import { ComponentStory } from '@storybook/react'
+
+import { Tooltip, TooltipProps } from './Tooltip'
+
+// More on default export: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
+export default {
+    title: 'Components/Tooltip',
+    component: Tooltip,
+}
+
+// More on component templates: https://storybook.js.org/docs/react/writing-stories/introduction#using-args
+const Template = (args: TooltipProps) => <Tooltip {...args} />
+
+export const Default: ComponentStory<typeof Tooltip> = Template.bind({})
+// More on args: https://storybook.js.org/docs/react/writing-stories/args
+Default.args = {
+    children: <p className="inline-block">hover me</p>,
+    label: 'Tooltip',
+}

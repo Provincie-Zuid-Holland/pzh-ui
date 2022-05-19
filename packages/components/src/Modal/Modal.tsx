fa80f4d60a29ea04a16a767b2b83c84bf04b3350
Aiden Buis <hello@aidenbuis.com>
2022-05-19 16:10:39 +0200

    Forgot overflow-hidden in container

--- packages/components/src/Modal/Modal.tsx
+++ packages/components/src/Modal/Modal.tsx
@@ -125,7 +125,7 @@ const ModalInner = ({
                 leaveFrom="opacity-100 translate-y-0 sm:scale-100"
                 leaveTo="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95">
                 <div
-                    className={`inline-block overflow-hidden text-left align-bottom transition-all transform bg-white rounded-[4px] shadow-xl sm:my-8 sm:align-middle sm:w-full ${maxWidth}`}>
+                    className={classNames(`inline-block text-left align-bottom transition-all transform bg-white rounded-[4px] shadow-xl sm:my-8 sm:align-middle sm:w-full ${maxWidth}`, {'overflow-hidden': !overflowVisible})}>
                     <div
                         className={classNames(`${containerPadding}`, {'overflow-y-auto': !overflowVisible, 'overflow-y-visible': overflowVisible})}
                         style={{

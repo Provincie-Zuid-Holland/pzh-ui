9a60e69c13f27afdb3fc90f8740ba096a14094c8
Stef Winterswijk <stef_winterswijk@hotmail.com>
2022-07-12 11:38:05 +0200

    Add loading state to Button

--- packages/components/src/Button/Button.tsx
+++ packages/components/src/Button/Button.tsx
@@ -1,5 +1,6 @@
 import React, { ReactNode } from 'react'
 import classNames from 'classnames'
+import { Spinner } from '@pzh-ui/icons'
 
 /**
  * Primary UI component for user interaction
@@ -11,6 +12,7 @@ export interface ButtonProps
     variant?: 'primary' | 'secondary' | 'cta'
     size?: 'large' | 'small'
     icon?: any
+    isLoading?: boolean
     children?: ReactNode
 }
 
@@ -20,45 +22,63 @@ export const Button = ({
     type = 'button',
     size = 'large',
     disabled,
-    icon: Icon,
+    icon,
+    isLoading,
     className,
     children,
     ...props
-}: ButtonProps) => (
-    <button
-        type={type}
-        className={classNames(
-            'pzh-button',
-            {
-                'px-4 pt-[15px] pb-[12px] h-[48px]': size === 'large',
-                'text-xs px-[12px] pb-[9px] pt-[12px] h-[36px]':
-                    size === 'small',
-                'bg-pzh-blue hover:bg-pzh-blue-dark text-white':
-                    variant === 'primary' && !disabled,
-                'text-pzh-blue border border-pzh-blue-dark border-opacity-35 hover:border-opacity-100':
-                    variant === 'secondary' && !disabled,
-                'bg-pzh-green hover:bg-pzh-green-dark text-white':
-                    variant === 'cta' && !disabled,
-                'cursor-pointer': !disabled,
-            },
-            className
-        )}
-        disabled={disabled}
-        {...props}>
-        {Icon ? (
-            <div className="flex items-center">
-                <Icon
-                    className={classNames({
-                        '-mt-[2px]': variant !== 'secondary',
-                        '-mt-0.5': variant === 'secondary',
-                        'mr-2': size === 'large',
-                        'mr-[8px]': size === 'small',
-                    })}
-                />
-                <span>{children || label}</span>
-            </div>
-        ) : (
-            children || label
-        )}
-    </button>
-)
+}: ButtonProps) => {
+    const Icon = icon && isLoading ? Spinner : icon ? icon : null
+
+    return (
+        <button
+            type={type}
+            className={classNames(
+                'pzh-button',
+                {
+                    'px-4 pt-[15px] pb-[12px] h-[48px]': size === 'large',
+                    'text-xs px-[12px] pb-[9px] pt-[12px] h-[36px]':
+                        size === 'small',
+                    'bg-pzh-blue hover:bg-pzh-blue-dark text-white':
+                        variant === 'primary' && !disabled,
+                    'text-pzh-blue border border-pzh-blue-dark border-opacity-35 hover:border-opacity-100':
+                        variant === 'secondary' && !disabled,
+                    'bg-pzh-green hover:bg-pzh-green-dark text-white':
+                        variant === 'cta' && !disabled,
+                    'cursor-pointer': !disabled,
+                },
+                className
+            )}
+            disabled={disabled}
+            {...props}>
+            {Icon ? (
+                <div className="flex items-center">
+                    <Icon
+                        className={classNames({
+                            '-mt-[2px]': variant !== 'secondary',
+                            '-mt-0.5': variant === 'secondary',
+                            'mr-2': size === 'large',
+                            'mr-[8px]': size === 'small',
+                            'animate-spin': isLoading,
+                        })}
+                    />
+                    <span>{children || label}</span>
+                </div>
+            ) : isLoading ? (
+                <div className="flex items-center">
+                    <Spinner
+                        className={classNames('animate-spin', {
+                            '-mt-[2px]': variant !== 'secondary',
+                            '-mt-0.5': variant === 'secondary',
+                            'mr-2': size === 'large',
+                            'mr-[8px]': size === 'small',
+                        })}
+                    />
+                    <span>{children || label}</span>
+                </div>
+            ) : (
+                children || label
+            )}
+        </button>
+    )
+}

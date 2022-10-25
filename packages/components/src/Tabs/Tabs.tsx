4b05a69d418f36fd48b175ca64dadd877fa101e7
Stef Winterswijk <stef_winterswijk@hotmail.com>
2022-10-25 15:31:04 +0200

    Update Tabs component

--- packages/components/src/Tabs/Tabs.tsx
+++ packages/components/src/Tabs/Tabs.tsx
@@ -53,10 +53,10 @@ function Tab({ item, state }: TabProps) {
         <div
             {...tabProps}
             ref={ref}
-            className={classNames('mr-4 -mb-0.5 border-b-2', {
-                'border-pzh-green text-pzh-green font-bold':
+            className={classNames('mr-4 -mb-0.5', {
+                'border-pzh-green border-b-[3px] text-pzh-green font-bold':
                     tabProps['aria-selected'],
-                'border-pzh-gray-400': !tabProps['aria-selected'],
+                'border-pzh-gray-400 border-b-2': !tabProps['aria-selected'],
             })}>
             {rendered}
         </div>

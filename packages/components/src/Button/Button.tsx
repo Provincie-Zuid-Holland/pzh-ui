69c061087db64974928d9eaa038b459618eb02fa
Stef Winterswijk <stef_winterswijk@hotmail.com>
2022-11-15 11:31:01 +0100

    Update Button component

--- packages/components/src/Button/Button.tsx
+++ packages/components/src/Button/Button.tsx
@@ -33,7 +33,7 @@ export const Button = ({
                 'pzh-button',
                 {
                     'px-4 pt-[15px] pb-[12px] h-[48px]': size === 'large',
-                    'text-xs px-[12px] pb-[9px] pt-[12px] h-[36px]':
+                    'text-xs px-[12px] pb-[9px] pt-[10px] h-[36px]':
                         size === 'small',
                     'bg-pzh-blue hover:bg-pzh-blue-dark text-white':
                         variant === 'primary' && !isDisabled,

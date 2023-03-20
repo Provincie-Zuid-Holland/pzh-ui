eec2e2d7b0674a607ea73102f0c6bf69c29c4c3b
Stef Winterswijk <stef_winterswijk@hotmail.com>
2023-03-20 14:00:24 +0100

    Update Text component

--- packages/components/src/Text/Text.tsx
+++ packages/components/src/Text/Text.tsx
@@ -1,3 +1,4 @@
+import classNames from 'classnames'
 import { ReactNode } from 'react'
 import { useWindowSize } from 'react-use'
 
@@ -9,6 +10,7 @@ export interface TextProps {
         | 'introduction-paragraph'
         | 'body'
         | 'body-small'
+        | 'body-bold'
     color?: string
     className?: string
     id?: string
@@ -20,62 +22,95 @@ export const Text = ({
     children,
     color = 'text-pzh-blue-dark',
     className = '',
-    id
+    id,
 }: TextProps) => {
     const windowSize = useWindowSize()
     const styles = getStylesForElement(windowSize, type)
 
-    if (type === 'quote') {
-        return (
-            <span
-                id={id}
-                data-testid="quote-span"
-                style={styles}
-                className={`${color} ${className}`}>
-                {children}
-            </span>
-        )
-    } else if (type === 'span') {
-        return (
-            <span id={id} style={styles} className={`${color} ${className}`}>
-                {children}
-            </span>
-        )
-    } else if (type === 'subtitle') {
-        return (
-            <p
-                id={id}
-                style={styles}
-                className={`text-base leading-normal ${color} ${
-                    className || 'inline-block'
-                }`}>
-                {children}
-            </p>
-        )
-    } else if (type === 'introduction-paragraph') {
-        return (
-            <p
-                id={id}
-                style={styles}
-                className={`${color} ${className || 'inline-block'}`}>
-                {children}
-            </p>
-        )
-    } else if (
-        type === 'body' ||
-        typeof type === 'undefined' ||
-        type === 'body-small'
-    ) {
-        return (
-            <p
-                id={id}
-                style={styles}
-                className={`${color} ${className || 'inline-block'}`}>
-                {children}
-            </p>
-        )
-    } else {
-        throw new Error(`${type} is not a valid text type`)
+    switch (type) {
+        case 'quote':
+            return (
+                <span
+                    id={id}
+                    data-testid="quote-span"
+                    style={styles}
+                    className={`${color} ${className}`}>
+                    {children}
+                </span>
+            )
+        case 'span':
+            return (
+                <span
+                    id={id}
+                    style={styles}
+                    className={`${color} ${className}`}>
+                    {children}
+                </span>
+            )
+        case 'subtitle':
+            return (
+                <p
+                    id={id}
+                    style={styles}
+                    className={classNames(
+                        'text-base leading-normal',
+                        color,
+                        {
+                            'inline-block': !className,
+                        },
+                        className
+                    )}>
+                    {children}
+                </p>
+            )
+        case 'introduction-paragraph':
+            return (
+                <p
+                    id={id}
+                    style={styles}
+                    className={classNames(
+                        color,
+                        {
+                            'inline-block': !className,
+                        },
+                        className
+                    )}>
+                    {children}
+                </p>
+            )
+        case 'body-bold':
+            return (
+                <p
+                    id={id}
+                    style={styles}
+                    className={classNames(
+                        'font-bold',
+                        color,
+                        {
+                            'inline-block': !className,
+                        },
+                        className
+                    )}>
+                    {children}
+                </p>
+            )
+        case 'body':
+        case 'body-small':
+        default:
+            return (
+                <p
+                    id={id}
+                    style={styles}
+                    className={classNames(
+                        color,
+                        {
+                            'inline-block': !className,
+                        },
+                        className
+                    )}>
+                    {children}
+                </p>
+            )
     }
 }
 
@@ -86,36 +121,39 @@ const getStylesForElement = (
     const smallScreen = 640
     const currentScreenIsMobile = windowSize.width <= smallScreen
 
-    if (type === 'quote') {
-        if (currentScreenIsMobile) {
-            return {
-                fontSize: '1.6rem',
-                lineHeight: '1.094rem',
+    switch (type) {
+        case 'quote':
+            if (currentScreenIsMobile) {
+                return {
+                    fontSize: '1.6rem',
+                    lineHeight: '1.094rem',
+                }
             }
-        } else {
+
             return {
                 fontSize: '1.667rem',
                 lineHeight: '1.2rem',
             }
-        }
-    } else if (type === 'introduction-paragraph') {
-        if (currentScreenIsMobile) {
-            return {
-                fontSize: '1.1rem',
-                lineHeight: '1.25rem',
+        case 'introduction-paragraph':
+            if (currentScreenIsMobile) {
+                return {
+                    fontSize: '1.1rem',
+                    lineHeight: '1.25rem',
+                }
             }
-        } else {
+
             return { fontSize: '1.2rem', lineHeight: '1.6rem' }
-        }
-    } else if (type === 'body' || type === 'span') {
-        return {
-            fontSize: '1rem',
-            lineHeight: '1.5rem',
-        }
-    } else if (type === 'body-small') {
-        return {
-            fontSize: '0.8rem',
-            lineHeight: '1rem',
-        }
+        case 'body-small':
+            return {
+                fontSize: '0.8rem',
+                lineHeight: '1rem',
+            }
+        case 'body':
+        case 'span':
+        default:
+            return {
+                fontSize: '1rem',
+                lineHeight: '1.5rem',
+            }
     }
 }

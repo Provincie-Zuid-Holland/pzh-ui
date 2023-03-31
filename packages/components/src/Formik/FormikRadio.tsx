6d2f142bcd1b4ad2898d1535e1c3f2af19621425
Stef Winterswijk <stef_winterswijk@hotmail.com>
2023-03-31 13:50:21 +0200

    Add FormikRadio

---
+++ packages/components/src/Formik/FormikRadio.tsx
@@ -0,0 +1,28 @@
+import { FastField, Field, FieldProps } from 'formik'
+
+import { FormikError } from './FormikError'
+import { FieldRadio, FieldRadioProps } from '../Form/FieldRadio'
+
+type FormikRadioProps = FieldRadioProps & {
+    name: string
+    optimized?: boolean
+}
+
+export const FormikRadio = ({
+    name,
+    optimized = true,
+    ...props
+}: FormikRadioProps) => {
+    const Component = optimized ? FastField : Field
+
+    return (
+        <>
+            <Component name={name}>
+                {({ field }: FieldProps<any>) => (
+                    <FieldRadio {...props} {...field} />
+                )}
+            </Component>
+            <FormikError name={name} />
+        </>
+    )
+}

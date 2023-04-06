6625bd3d75660cfd048e7914acfdcd0374e71d89
Stef Winterswijk <stef_winterswijk@hotmail.com>
2023-04-06 09:03:06 +0200

    Update Breadcrumbs

--- packages/components/src/Breadcrumbs/Breadcrumbs.tsx
+++ packages/components/src/Breadcrumbs/Breadcrumbs.tsx
@@ -18,12 +18,14 @@ export const Breadcrumbs = ({ className, items = [] }: BreadcrumbsProps) => (
         <ol className="hidden md:flex">
             {items.map((item, index) => {
                 return index === items.length - 1 ? (
-                    <li key={item.name} className="inline-block truncate">
+                    <li
+                        key={item.name + index}
+                        className="inline-block truncate">
                         <span aria-current="page">{item.name}</span>
                     </li>
                 ) : (
                     <li
-                        key={item.name}
+                        key={item.name + index}
                         className="flex items-center mr-[8px] underline underline-offset-2 whitespace-nowrap">
                         <Link to={item.path} className="mr-[8px]">
                             {item.name}

38ead19f770d42c512c4e65a42952aa0ae28e12d
Stef Winterswijk <stef_winterswijk@hotmail.com>
2023-04-13 16:11:32 +0200

    Update ListLink component

--- packages/components/src/ListLink/ListLink.tsx
+++ packages/components/src/ListLink/ListLink.tsx
@@ -1,15 +1,23 @@
 import { Link } from 'react-router-dom'
 import { AngleRight } from '@pzh-ui/icons'
+import classNames from 'classnames'
 
 export interface ListLinkProps {
     text: string
     to: string
+    className?: string
 }
 
-export const ListLink = ({ text, to }: ListLinkProps) => {
+export const ListLink = ({ text, to, className }: ListLinkProps) => {
     return (
         <Link
-            className="inline-flex items-start underline decoration-1 text-pzh-blue-dark hover:text-pzh-green"
+            className={classNames(
+                'inline-flex items-start underline decoration-1 ',
+                {
+                    'text-pzh-blue-dark hover:text-pzh-green': !className,
+                },
+                className
+            )}
             to={to}>
             <AngleRight className="mr-2 relative -bottom-[6px]" />
             <span>{text}</span>

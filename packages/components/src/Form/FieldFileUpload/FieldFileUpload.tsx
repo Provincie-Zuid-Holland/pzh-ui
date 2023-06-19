a95dfc132b75ccd0b1137dc506f0a23266ce6ea2
Stef Winterswijk <stef_winterswijk@hotmail.com>
2023-06-19 11:25:06 +0200

    Update FieldFileUpload

--- packages/components/src/Form/FieldFileUpload/FieldFileUpload.tsx
+++ packages/components/src/Form/FieldFileUpload/FieldFileUpload.tsx
@@ -127,16 +127,7 @@ export const FieldFileUpload = ({
                     })}
                 />
             )}
-            <div
-                className={classNames(
-                    'relative py-6 text-pzh-blue-dark text-center border border-dashed border-pzh-gray-600 rounded-[4px] cursor-pointer',
-                    className,
-                    {
-                        'md:col-span-4 col-span-6': layout === 'grid',
-                        hidden: myFiles.length === props.maxFiles,
-                    }
-                )}
-                {...getRootProps()}>
+            <div className="relative">
                 <input
                     {...getInputProps({ required, name })}
                     style={{
@@ -150,97 +141,113 @@ export const FieldFileUpload = ({
                     }}
                 />
 
-                <div>
-                    <CloudArrowUp
-                        size={60}
-                        className={classNames('mb-2 mx-auto max-w-[2rem]', {
-                            'text-pzh-blue-dark': !isDragActive,
-                            'text-pzh-green': isDragActive && !isDragReject,
-                            'text-pzh-red': isDragReject && isDragActive,
-                        })}
-                    />
+                <div
+                    className={classNames(
+                        'py-6 text-pzh-blue-dark text-center border border-dashed border-pzh-gray-600 rounded-[4px] cursor-pointer',
+                        className,
+                        {
+                            'md:col-span-4 col-span-6': layout === 'grid',
+                            hidden: myFiles.length === props.maxFiles,
+                        }
+                    )}
+                    {...getRootProps()}>
+                    <div>
+                        <CloudArrowUp
+                            size={60}
+                            className={classNames('mb-2 mx-auto max-w-[2rem]', {
+                                'text-pzh-blue-dark': !isDragActive,
+                                'text-pzh-green': isDragActive && !isDragReject,
+                                'text-pzh-red': isDragReject && isDragActive,
+                            })}
+                        />
 
-                    <p>
-                        {!isDragReject ? (
-                            <>
-                                Sleep hier je bestanden naartoe of{' '}
-                                {!isDragActive && (
-                                    <span className="text-pzh-green underline">
-                                        blader door je bestanden
-                                    </span>
-                                )}
-                            </>
-                        ) : (
-                            <>Dit bestandstype is niet toegestaan.</>
-                        )}
-                    </p>
-                    <span className="text-pzh-gray-600">
-                        {Array.isArray(acceptedTypes) &&
-                        acceptedTypes.length > 0 ? (
-                            <>
-                                De ondersteunde bestandstypen zijn{' '}
-                                {acceptedTypes
-                                    .join(', ')
-                                    .replace(/,(?!.*,)/gim, ' en')}
-                                . Maximale bestandsgrootte{' '}
-                                {formatBytes(maxSize)}.
-                            </>
-                        ) : (
-                            <>
-                                Het ondersteunde bestandstype is {accept}.
-                                Maximale bestandsgrootte {formatBytes(maxSize)}.
-                            </>
-                        )}
-                    </span>
-                </div>
-            </div>
-            {!!myFiles.length && (
-                <ul>
-                    {myFiles.map((file, index) => (
-                        <li
-                            key={file.path || `file-${index}`}
-                            className="pzh-form-input mt-2 overflow-hidden"
-                            style={preview ? { paddingBottom: 0 } : undefined}>
-                            <div
-                                className={classNames(
-                                    'flex justify-between items-center px-4',
-                                    {
-                                        'pb-1': preview,
-                                    }
-                                )}>
-                                <span>{file.path || label}</span>
-                                <div className="flex items-center nowrap">
-                                    <span className="text-[16px] text-pzh-blue-dark/50">
-                                        {formatBytes(file.size)}
-                                    </span>
-                                    <button
-                                        type="button"
-                                        onClick={removeFile(file)}>
-                                        <TrashCan
-                                            size={16}
-                                            className="ml-4 -mt-[2px] text-pzh-red"
-                                        />
-                                        <span className="sr-only">
-                                            Verwijderen
+                        <p>
+                            {!isDragReject ? (
+                                <>
+                                    Sleep hier je bestanden naartoe of{' '}
+                                    {!isDragActive && (
+                                        <span className="text-pzh-green underline">
+                                            blader door je bestanden
                                         </span>
-                                    </button>
-                                </div>
-                            </div>
-                            {file.preview && (
-                                <div className="border-t border-pzh-gray-600">
-                                    <img
-                                        src={file.preview}
-                                        alt={file.name}
-                                        onLoad={() =>
-                                            URL.revokeObjectURL(file.preview!)
-                                        }
-                                    />
-                                </div>
+                                    )}
+                                </>
+                            ) : (
+                                <>Dit bestandstype is niet toegestaan.</>
                             )}
-                        </li>
-                    ))}
-                </ul>
-            )}
+                        </p>
+                        <span className="text-pzh-gray-600">
+                            {Array.isArray(acceptedTypes) &&
+                            acceptedTypes.length > 0 ? (
+                                <>
+                                    De ondersteunde bestandstypen zijn{' '}
+                                    {acceptedTypes
+                                        .join(', ')
+                                        .replace(/,(?!.*,)/gim, ' en')}
+                                    . Maximale bestandsgrootte{' '}
+                                    {formatBytes(maxSize)}.
+                                </>
+                            ) : (
+                                <>
+                                    Het ondersteunde bestandstype is {accept}.
+                                    Maximale bestandsgrootte{' '}
+                                    {formatBytes(maxSize)}.
+                                </>
+                            )}
+                        </span>
+                    </div>
+                </div>
+                {!!myFiles.length && (
+                    <ul>
+                        {myFiles.map((file, index) => (
+                            <li
+                                key={file.path || `file-${index}`}
+                                className="pzh-form-input mt-2 overflow-hidden"
+                                style={
+                                    preview ? { paddingBottom: 0 } : undefined
+                                }>
+                                <div
+                                    className={classNames(
+                                        'flex justify-between items-center px-4',
+                                        {
+                                            'pb-1': preview,
+                                        }
+                                    )}>
+                                    <span>{file.path || label}</span>
+                                    <div className="flex items-center nowrap">
+                                        <span className="text-[16px] text-pzh-blue-dark/50">
+                                            {formatBytes(file.size)}
+                                        </span>
+                                        <button
+                                            type="button"
+                                            onClick={removeFile(file)}>
+                                            <TrashCan
+                                                size={16}
+                                                className="ml-4 -mt-[2px] text-pzh-red"
+                                            />
+                                            <span className="sr-only">
+                                                Verwijderen
+                                            </span>
+                                        </button>
+                                    </div>
+                                </div>
+                                {file.preview && (
+                                    <div className="border-t border-pzh-gray-600">
+                                        <img
+                                            src={file.preview}
+                                            alt={file.name}
+                                            onLoad={() =>
+                                                URL.revokeObjectURL(
+                                                    file.preview!
+                                                )
+                                            }
+                                        />
+                                    </div>
+                                )}
+                            </li>
+                        ))}
+                    </ul>
+                )}
+            </div>
         </div>
     )
 }

3e92e5e17b304ec5ef0a45df6b434477177f47f2
Stef Winterswijk <stef_winterswijk@hotmail.com>
2023-08-29 14:41:04 +0200

    Update config

--- packages/config/prettier/index.js
+++ packages/config/prettier/index.js
@@ -1,9 +1,20 @@
 module.exports = {
-    plugins: ['prettier-plugin-tailwindcss'],
+    plugins: [
+        '@trivago/prettier-plugin-sort-imports',
+        'prettier-plugin-tailwindcss',
+    ],
     trailingComma: 'es5',
     tabWidth: 4,
     semi: false,
     arrowParens: 'avoid',
     singleQuote: true,
     bracketSameLine: true,
+    importOrder: [
+        '<THIRD_PARTY_MODULES>',
+        '^@pzh-ui/(.*)$',
+        '^@/(.*)$',
+        '^[./]',
+    ],
+    importOrderSeparation: true,
+    importOrderSortSpecifiers: true,
 }

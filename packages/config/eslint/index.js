e809f3a1db0a15ccf53aef0215c84cbcea14dc44
Stef Winterswijk <stef_winterswijk@hotmail.com>
2023-11-07 09:39:58 +0100

    Update eslint config

--- packages/config/eslint/index.js
+++ packages/config/eslint/index.js
@@ -70,6 +70,27 @@ module.exports = {
         'react-hooks/rules-of-hooks': 'error',
         'react-hooks/exhaustive-deps': 'warn',
         'react/no-unescaped-entities': 'off',
+        'import/no-unresolved': 'error',
+        'import/order': [
+            'error',
+            {
+                groups: [
+                    'builtin', // Built-in imports (come from NodeJS native) go first
+                    'external', // <- External imports
+                    'internal', // <- Absolute imports
+                    ['sibling', 'parent'], // <- Relative imports, the sibling and parent types they can be mingled together
+                    'index', // <- index imports
+                    'unknown', // <- unknown
+                ],
+                'newlines-between': 'always',
+                alphabetize: {
+                    /* sort in ascending order. Options: ["ignore", "asc", "desc"] */
+                    order: 'asc',
+                    /* ignore case. Options: [true, false] */
+                    caseInsensitive: true,
+                },
+            },
+        ],
     },
     overrides: [
         {

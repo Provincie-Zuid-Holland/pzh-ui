c729325942ca217a0894959b7958fddee697a9a1
Stef Winterswijk <stef_winterswijk@hotmail.com>
2023-11-29 16:56:38 +0100

    Add type button to options

--- packages/components/src/Form/FieldRte/components/TableMenu/TableMenu.tsx
+++ packages/components/src/Form/FieldRte/components/TableMenu/TableMenu.tsx
@@ -55,29 +55,34 @@ const TableMenu = ({ editor, setRightClick }: TableMenuProps) => {
             ref={ref}
             className="bg-pzh-white after:content-[`' '`] after:border-b-pzh-white flex flex-col gap-2 px-6 py-4 shadow-[0_1px_8px_0_rgba(0,0,0,0.2)] after:absolute after:-top-2 after:left-0 after:right-0 after:mx-auto after:block after:h-0 after:w-0 after:border-x-[10px] after:border-b-[10px] after:border-t-0 after:border-x-[transparent] after:border-t-transparent">
             <button
+                type="button"
                 onClick={() => editor.chain().focus().addRowAfter().run()}
                 className="text-pzh-blue text-left font-bold">
                 Rij onder invoegen
             </button>
             <Divider className="bg-pzh-gray-600" />
             <button
+                type="button"
                 onClick={() => editor.chain().focus().addColumnBefore().run()}
                 className="text-pzh-blue text-left font-bold">
                 Kolom links invoegen
             </button>
             <button
+                type="button"
                 onClick={() => editor.chain().focus().addColumnAfter().run()}
                 className="text-pzh-blue text-left font-bold">
                 Kolom rechts invoegen
             </button>
             <Divider className="bg-pzh-gray-600" />
             <button
+                type="button"
                 onClick={() => editor.chain().focus().deleteRow().run()}
                 className="text-pzh-blue text-left font-bold">
                 Rij verwijderen
             </button>
             {canDeleteCol && (
                 <button
+                    type="button"
                     onClick={() => editor.chain().focus().deleteColumn().run()}
                     className="text-pzh-blue text-left font-bold">
                     Kolom verwijderen
@@ -85,6 +90,7 @@ const TableMenu = ({ editor, setRightClick }: TableMenuProps) => {
             )}
             <Divider className="bg-pzh-gray-600" />
             <button
+                type="button"
                 onClick={() => editor.chain().focus().toggleHeaderRow().run()}
                 className="text-pzh-blue text-left font-bold">
                 Headerrij aan/uit

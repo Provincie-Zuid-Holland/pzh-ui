ea58478e1b610bbabf4a335cf1e4fe0d86dd5040
Stef Winterswijk <stef_winterswijk@hotmail.com>
2024-04-02 13:00:29 +0200

    Update vite config

--- packages/icons/vite.config.ts
+++ packages/icons/vite.config.ts
@@ -1,10 +1,12 @@
 import typescriptPlugin from "@rollup/plugin-typescript";
 import react from "@vitejs/plugin-react";
+import path from "path";
 import { visualizer } from "rollup-plugin-visualizer";
+import { PluginOption, defineConfig } from "vite";
 
-import { getClientConfiguration } from "../config/vite";
+const isExternal = (id) => !id.startsWith(".") && !path.isAbsolute(id);
 
-export default getClientConfiguration({
+export default defineConfig({
   plugins: [
     react({
       jsxRuntime: "classic",
@@ -15,16 +17,24 @@ export default getClientConfiguration({
     visualizer(),
   ],
   build: {
+    target: "esnext",
+    sourcemap: true,
+    emptyOutDir: true,
     rollupOptions: {
+      external: isExternal,
       plugins: [
         typescriptPlugin({
           exclude: ["**/*.test.tsx", "**/*.stories.tsx"],
-        }),
+        }) as PluginOption,
       ],
       output: {
         entryFileNames: "[name].js",
         preserveModules: true,
       },
+    },
+    lib: {
+      entry: path.resolve(path.resolve(path.dirname("")), "src/index.ts"),
+      formats: ["es"],
     },
   },
 });

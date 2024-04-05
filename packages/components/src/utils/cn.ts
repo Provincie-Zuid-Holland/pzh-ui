90c25e7a7f3af2db3723ebb39f183c78597c122d
Stef Winterswijk <stef_winterswijk@hotmail.com>
2024-04-05 16:07:27 +0200

    Update cn utility

--- packages/components/src/utils/cn.ts
+++ packages/components/src/utils/cn.ts
@@ -1,5 +1,14 @@
 import { clsx, type ClassValue } from 'clsx'
-import { twMerge } from 'tailwind-merge'
+import { extendTailwindMerge } from 'tailwind-merge'
+
+const twMerge = extendTailwindMerge({
+    // use the `extend` key in case you want to extend instead of override
+    override: {
+        classGroups: {
+            'font-size': ['text-s', 'text-m', 'text-l'],
+        },
+    },
+})
 
 export function cn(...inputs: ClassValue[]) {
     return twMerge(clsx(inputs))

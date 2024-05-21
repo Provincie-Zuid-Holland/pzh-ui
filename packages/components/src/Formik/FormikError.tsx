69523e1e9791f00f427a66f18ceb09335ad55f9a
Stef Winterswijk <stef_winterswijk@hotmail.com>
2024-05-21 16:37:48 +0200

    Update dependencies

--- packages/components/src/Formik/FormikError.tsx
+++ packages/components/src/Formik/FormikError.tsx
@@ -7,13 +7,11 @@ interface FormikErrorProps {
 export const FormikError = ({ name }: FormikErrorProps) => {
     return (
         <ErrorMessage name={name}>
-            {(message: string) => {
-                return (
-                    <span className="text-pzh-red text-s mt-1 block">
-                        {message}
-                    </span>
-                )
-            }}
+            {(message: string) => (
+                <span className="text-pzh-red text-s mt-1 block">
+                    {message}
+                </span>
+            )}
         </ErrorMessage>
     )
 }

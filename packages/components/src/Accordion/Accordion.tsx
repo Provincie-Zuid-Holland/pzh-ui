52c5154c881a6e83014c1c0f71b1091cbc0dab29
Stef Winterswijk <stef_winterswijk@hotmail.com>
2024-08-28 15:33:55 +0200

    Update Accordion component

--- packages/components/src/Accordion/Accordion.tsx
+++ packages/components/src/Accordion/Accordion.tsx
@@ -20,8 +20,10 @@ AccordionItem.displayName = 'AccordionItem'
 
 const AccordionTrigger = React.forwardRef<
     React.ElementRef<typeof AccordionPrimitive.Trigger>,
-    React.ComponentPropsWithoutRef<typeof AccordionPrimitive.Trigger>
->(({ className, children, ...props }, ref) => (
+    React.ComponentPropsWithoutRef<typeof AccordionPrimitive.Trigger> & {
+        hideIcon?: boolean
+    }
+>(({ className, children, hideIcon, ...props }, ref) => (
     <AccordionPrimitive.Header className="flex">
         <AccordionPrimitive.Trigger
             ref={ref}
@@ -31,10 +33,12 @@ const AccordionTrigger = React.forwardRef<
             )}
             {...props}>
             {children}
-            <AngleDown
-                className="h-4 w-4 shrink-0 transition-transform duration-200"
-                color="text-pzh-blue-900"
-            />
+            {!hideIcon && (
+                <AngleDown
+                    className="h-4 w-4 shrink-0 transition-transform duration-200"
+                    color="text-pzh-blue-900"
+                />
+            )}
         </AccordionPrimitive.Trigger>
     </AccordionPrimitive.Header>
 ))

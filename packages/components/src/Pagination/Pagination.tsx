cb672800540cbfdee27bc171b4328b958e7a8ea0
Stef Winterswijk <stef_winterswijk@hotmail.com>
2024-10-02 11:56:50 +0200

    Update Pagination

--- packages/components/src/Pagination/Pagination.tsx
+++ packages/components/src/Pagination/Pagination.tsx
@@ -1,9 +1,10 @@
+import { AngleLeft, AngleRight } from '@pzh-ui/icons'
 import { useMemo } from 'react'
 import ResponsivePagination, {
     ResponsivePaginationProps,
 } from 'react-responsive-pagination'
 
-import { AngleLeft, AngleRight } from '@pzh-ui/icons'
+import { cn } from '../utils'
 
 export interface PaginationProps extends ResponsivePaginationProps {
     /** Items per page */
@@ -13,6 +14,7 @@ export interface PaginationProps extends ResponsivePaginationProps {
 export const Pagination = ({
     limit = 20,
     total = 0,
+    className,
     ...rest
 }: PaginationProps) => {
     const pageCount = useMemo(() => Math.ceil(total / limit), [limit, total])
@@ -20,14 +22,17 @@ export const Pagination = ({
     return (
         <ResponsivePagination
             data-testid="pagination"
-            nextLabel={<AngleRight size={18} />}
-            nextClassName="w-10 h-10 flex items-center justify-center focus:outline-none focus:ring focus:ring-pzh-focus ring-offset-2 rounded"
+            nextLabel={<AngleRight size={18} className="-mt-1" />}
+            nextClassName="mr-0 min-w-10 min-h-10 flex items-center justify-center focus:outline-none focus:ring focus:ring-pzh-focus ring-offset-2 rounded"
             previousLabel={<AngleLeft size={18} />}
-            previousClassName="w-10 h-10 flex items-center justify-center focus:outline-none focus:ring focus:ring-pzh-focus ring-offset-2 rounded"
-            className="text-pzh-blue flex items-center gap-2 font-bold"
+            previousClassName="min-w-10 min-h-10 ml-0 flex items-center justify-center focus:outline-none focus:ring focus:ring-pzh-focus ring-offset-2 rounded"
+            className={cn(
+                'text-pzh-blue flex items-center justify-center font-bold',
+                className
+            )}
             disabledItemClassName="text-pzh-gray-400 pointer-events-none"
-            pageLinkClassName="w-full h-full pt-1 flex items-center justify-center rounded focus:outline-none focus:ring focus:ring-pzh-focus ring-offset-2"
-            pageItemClassName="w-10 h-10 rounded"
+            pageLinkClassName="mt-px w-full h-full leading-10 flex items-center justify-center rounded focus:outline-none focus:ring focus:ring-pzh-focus ring-offset-2"
+            pageItemClassName="min-w-10 min-h-10 h-10 rounded mx-1"
             activeItemClassName="border border-pzh-blue"
             {...rest}
             total={pageCount}

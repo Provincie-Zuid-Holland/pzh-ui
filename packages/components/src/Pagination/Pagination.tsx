8479333486f6c3497d62075f3099e3f21ec46b1c
Stef Winterswijk <stef_winterswijk@hotmail.com>
2024-10-02 12:03:28 +0200

    Update Pagination

--- packages/components/src/Pagination/Pagination.tsx
+++ packages/components/src/Pagination/Pagination.tsx
@@ -23,17 +23,17 @@ export const Pagination = ({
         <ResponsivePagination
             data-testid="pagination"
             nextLabel={<AngleRight size={18} className="-mt-1" />}
-            nextClassName="mr-0 min-w-10 min-h-10 flex items-center justify-center focus:outline-none focus:ring focus:ring-pzh-focus ring-offset-2 rounded"
+            nextClassName="mr-0 min-w-10 min-h-10"
             previousLabel={<AngleLeft size={18} />}
-            previousClassName="min-w-10 min-h-10 ml-0 flex items-center justify-center focus:outline-none focus:ring focus:ring-pzh-focus ring-offset-2 rounded"
+            previousClassName="min-w-10 min-h-10 ml-0"
             className={cn(
                 'text-pzh-blue flex items-center justify-center font-bold',
                 className
             )}
             disabledItemClassName="text-pzh-gray-400 pointer-events-none"
-            pageLinkClassName="mt-px w-full h-full leading-10 flex items-center justify-center rounded focus:outline-none focus:ring focus:ring-pzh-focus ring-offset-2"
+            pageLinkClassName="w-full h-10 leading-10 flex items-center justify-center rounded focus:outline-none focus:ring focus:ring-pzh-focus ring-offset-2"
             pageItemClassName="min-w-10 min-h-10 h-10 rounded mx-1"
-            activeItemClassName="border border-pzh-blue"
+            activeItemClassName="[&_a]:border [&_a]:border-pzh-blue"
             {...rest}
             total={pageCount}
         />

ce53e863bcb4d9ede9a9c098b998d28cc16ea96b
Stef Winterswijk <stef_winterswijk@hotmail.com>
2025-01-29 12:00:35 +0100

    Update tailwind

--- packages/css/config/custom.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-    presets: [require('./index')],
-    content: ['../components/src/**/*.{js,jsx,ts,tsx}'],
-}
---
+++ packages/css/config/custom.ts
@@ -0,0 +1,6 @@
+import preset from './index'
+
+export default {
+    presets: [preset],
+    content: ['../components/src/**/*.{js,jsx,ts,tsx}'],
+}

628ec36254691c89f6bf42ceb0510684beee3d2a
Stef Winterswijk <stef_winterswijk@hotmail.com>
2025-01-29 10:46:05 +0100

    Update eslint config

--- packages/config/eslint/index.js
+++ packages/config/eslint/index.js
@@ -1,121 +1,119 @@
-export default [
-  {
-    parser: "@babel/eslint-parser",
+export default {
+    parser: '@babel/eslint-parser',
     parserOptions: {
-      requireConfigFile: false,
-      babelOptions: {
-        presets: ["@babel/preset-react"],
-      },
+        requireConfigFile: false,
+        babelOptions: {
+            presets: ['@babel/preset-react'],
+        },
     },
     env: {
-      browser: true,
-      commonjs: true,
-      es6: true,
-      jest: true,
-      node: true,
+        browser: true,
+        commonjs: true,
+        es6: true,
+        jest: true,
+        node: true,
     },
-    plugins: ["react-hooks"],
+    plugins: ['react-hooks'],
     settings: {
-      react: {
-        version: "detect",
-      },
+        react: {
+            version: 'detect',
+        },
     },
     extends: [
-      "plugin:prettier/recommended",
-      "plugin:import/errors",
-      "plugin:import/warnings",
-      "plugin:import/typescript",
-      "plugin:@typescript-eslint/eslint-recommended",
-      "plugin:@typescript-eslint/recommended",
-      "plugin:jsx-a11y/recommended",
-      "prettier",
+        'plugin:prettier/recommended',
+        'plugin:import/errors',
+        'plugin:import/warnings',
+        'plugin:import/typescript',
+        'plugin:@typescript-eslint/eslint-recommended',
+        'plugin:@typescript-eslint/recommended',
+        'plugin:jsx-a11y/recommended',
+        'prettier',
     ],
     rules: {
-      "@typescript-eslint/no-var-requires": "off",
-      "@typescript-eslint/explicit-function-return-type": "off",
-      "@typescript-eslint/no-non-null-assertion": "off",
-      "@typescript-eslint/no-explicit-any": "off",
-      "@typescript-eslint/no-use-before-define": "off",
-      "@typescript-eslint/explicit-module-boundary-types": "off",
-      "@typescript-eslint/ban-ts-comment": "off",
-      "@typescript-eslint/no-unused-vars": [
-        "warn",
-        {
-          argsIgnorePattern: "^_|req|res|next|err|ctx|args|context|info",
-        },
-      ],
-      "@typescript-eslint/no-object-literal-type-assertion": "off",
-      "@typescript-eslint/explicit-member-accessibility": "off",
-      "@typescript-eslint/camelcase": "off",
-      "@typescript-eslint/no-empty-function": "off",
-      "@typescript-eslint/no-empty-interface": "off",
-      "@typescript-eslint/ban-ts-ignore": "off",
-      "@typescript-eslint/no-this-alias": "off",
-      "prettier/prettier": "off",
-      "import/no-named-as-default": "off",
-      "import/no-named-as-default-member": "off",
-      "import/default": "off",
-      "import/named": "off",
-      "import/namespace": "off",
-      "jsx-a11y/click-events-have-key-events": "off",
-      "jsx-a11y/anchor-is-valid": "off",
-      "jsx-a11y/no-static-element-interactions": "off",
-      "jsx-a11y/no-noninteractive-element-interactions": "off",
-      "jsx-a11y/no-noninteractive-tabindex": "off",
-      "jsx-a11y/no-onchange": "off",
-      "no-control-regex": 0,
-      "react/prop-types": "off",
-      "react/react-in-jsx-scope": "off",
-      "react/display-name": "off",
-      "react-hooks/rules-of-hooks": "error",
-      "react-hooks/exhaustive-deps": "warn",
-      "react/no-unescaped-entities": "off",
-      "import/no-unresolved": "error",
-      "import/order": [
-        "warn",
-        {
-          groups: [
-            "builtin", // Built-in imports (come from NodeJS native) go first
-            "external", // <- External imports
-            "internal", // <- Absolute imports
-            ["sibling", "parent"], // <- Relative imports, the sibling and parent types they can be mingled together
-            "index", // <- index imports
-            "unknown", // <- unknown
-          ],
-          "newlines-between": "always",
-          alphabetize: {
-            /* sort in ascending order. Options: ["ignore", "asc", "desc"] */
-            order: "asc",
-            /* ignore case. Options: [true, false] */
-            caseInsensitive: true,
-          },
-        },
-      ],
+        '@typescript-eslint/no-var-requires': 'off',
+        '@typescript-eslint/explicit-function-return-type': 'off',
+        '@typescript-eslint/no-non-null-assertion': 'off',
+        '@typescript-eslint/no-explicit-any': 'off',
+        '@typescript-eslint/no-use-before-define': 'off',
+        '@typescript-eslint/explicit-module-boundary-types': 'off',
+        '@typescript-eslint/ban-ts-comment': 'off',
+        '@typescript-eslint/no-unused-vars': [
+            'warn',
+            {
+                argsIgnorePattern: '^_|req|res|next|err|ctx|args|context|info',
+            },
+        ],
+        '@typescript-eslint/no-object-literal-type-assertion': 'off',
+        '@typescript-eslint/explicit-member-accessibility': 'off',
+        '@typescript-eslint/camelcase': 'off',
+        '@typescript-eslint/no-empty-function': 'off',
+        '@typescript-eslint/no-empty-interface': 'off',
+        '@typescript-eslint/ban-ts-ignore': 'off',
+        '@typescript-eslint/no-this-alias': 'off',
+        'prettier/prettier': 'off',
+        'import/no-named-as-default': 'off',
+        'import/no-named-as-default-member': 'off',
+        'import/default': 'off',
+        'import/named': 'off',
+        'import/namespace': 'off',
+        'jsx-a11y/click-events-have-key-events': 'off',
+        'jsx-a11y/anchor-is-valid': 'off',
+        'jsx-a11y/no-static-element-interactions': 'off',
+        'jsx-a11y/no-noninteractive-element-interactions': 'off',
+        'jsx-a11y/no-noninteractive-tabindex': 'off',
+        'jsx-a11y/no-onchange': 'off',
+        'no-control-regex': 0,
+        'react/prop-types': 'off',
+        'react/react-in-jsx-scope': 'off',
+        'react/display-name': 'off',
+        'react-hooks/rules-of-hooks': 'error',
+        'react-hooks/exhaustive-deps': 'warn',
+        'react/no-unescaped-entities': 'off',
+        'import/no-unresolved': 'error',
+        'import/order': [
+            'warn',
+            {
+                groups: [
+                    'builtin', // Built-in imports (come from NodeJS native) go first
+                    'external', // <- External imports
+                    'internal', // <- Absolute imports
+                    ['sibling', 'parent'], // <- Relative imports, the sibling and parent types they can be mingled together
+                    'index', // <- index imports
+                    'unknown', // <- unknown
+                ],
+                'newlines-between': 'always',
+                alphabetize: {
+                    /* sort in ascending order. Options: ["ignore", "asc", "desc"] */
+                    order: 'asc',
+                    /* ignore case. Options: [true, false] */
+                    caseInsensitive: true,
+                },
+            },
+        ],
     },
     overrides: [
-      {
-        files: ["**/*.ts?(x)"],
-        parser: "@typescript-eslint/parser",
-        parserOptions: {
-          ecmaVersion: 2018,
-          sourceType: "module",
-          ecmaFeatures: {
-            jsx: true,
-          },
-          // typescript-eslint specific options
-          warnOnUnsupportedTypeScriptVersion: true,
-        },
-        settings: {
-          "import/parsers": {
-            "@typescript-eslint/parser": [".ts", ".tsx"],
-          },
-          "import/resolver": {
-            typescript: {
-              alwaysTryTypes: true,
+        {
+            files: ['**/*.ts?(x)'],
+            parser: '@typescript-eslint/parser',
+            parserOptions: {
+                ecmaVersion: 2018,
+                sourceType: 'module',
+                ecmaFeatures: {
+                    jsx: true,
+                },
+                // typescript-eslint specific options
+                warnOnUnsupportedTypeScriptVersion: true,
             },
-          },
+            settings: {
+                'import/parsers': {
+                    '@typescript-eslint/parser': ['.ts', '.tsx'],
+                },
+                'import/resolver': {
+                    typescript: {
+                        alwaysTryTypes: true,
+                    },
+                },
+            },
         },
-      },
     ],
-  },
-];
+}
--- packages/config/prettier/index.js
+++ packages/config/prettier/index.js
@@ -1,9 +1,12 @@
 module.exports = {
-  plugins: ["prettier-plugin-organize-imports", "prettier-plugin-tailwindcss"],
-  trailingComma: "es5",
-  tabWidth: 4,
-  semi: false,
-  arrowParens: "avoid",
-  singleQuote: true,
-  bracketSameLine: true,
-};
+    plugins: [
+        'prettier-plugin-organize-imports',
+        'prettier-plugin-tailwindcss',
+    ],
+    trailingComma: 'es5',
+    tabWidth: 4,
+    semi: false,
+    arrowParens: 'avoid',
+    singleQuote: true,
+    bracketSameLine: true,
+}

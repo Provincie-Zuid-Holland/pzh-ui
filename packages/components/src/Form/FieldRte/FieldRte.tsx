e013eee358d0e64577f383bbcbff449848ecae2d
AdilAlTamimi <110891271+AdilAlTamimi@users.noreply.github.com>
2025-08-20 16:29:45 +0200

    make tablemenu customisable in fieldrte

--- packages/components/src/Form/FieldRte/FieldRte.tsx
+++ packages/components/src/Form/FieldRte/FieldRte.tsx
@@ -29,6 +29,7 @@ import { ReactNode, useEffect, useState } from 'react'
 import { cn } from '../../utils'
 import { FieldLabel } from '../FieldLabel'
 import RteMenuBar from './components/RteMenuBar'
+import { TableMenuOption } from './components/TableMenu/TableMenu'
 import { HandleDOMEvents } from './extensions/handleDOMEvents'
 import ImageUpload from './extensions/imageUpload'
 import { NestedListLimit } from './extensions/limitNestedLists'
@@ -65,6 +66,8 @@ export interface FieldRteProps {
     customMenuButtons?: (editor: Editor) => ReactNode[]
     /** List of custom extensions */
     customExtensions?: AnyExtension[]
+    /** List of custom context-menu options when editing tables */
+    customTableMenuOptions?: TableMenuOption[]
     /** Classnames of Tiptap editor */
     className?: string
     /** Classnames of menu */
@@ -83,8 +86,6 @@ export interface FieldRteProps {
             maxSize?: number
         }
     }
-    /** Enable Color picker if Table opion is enabled */
-    enableColorPicker?: boolean
 }
 
 export type TextEditorMenuOptions =
@@ -132,7 +133,7 @@ export const FieldRte = ({
             maxSize: 1048576,
         },
     },
-    enableColorPicker = false,
+    customTableMenuOptions,
 }: FieldRteProps) => {
     const [rightClick, setRightClick] = useState(false)
 
@@ -347,7 +348,7 @@ export const FieldRte = ({
                         imageOptions={imageOptions}
                         rightClick={rightClick}
                         setRightClick={setRightClick}
-                        enableColorPicker={enableColorPicker}
+                        tableMenuOptions={customTableMenuOptions}
                     />
                     <EditorContent editor={editor} />
                 </div>

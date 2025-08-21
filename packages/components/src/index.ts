eca673ebb25e919c7f6bcab1789f277360d36fdd
Stef Winterswijk <stef_winterswijk@hotmail.com>
2025-08-21 13:39:46 +0200

    Update barrel file

--- packages/components/src/index.ts
+++ packages/components/src/index.ts
@@ -1,36 +1,152 @@
+// ───────────────────────────────────────────────
 // Components
-export * from './Accordion'
-export * from './BackLink'
-export * from './Badge'
-export * from './Banner'
-export * from './Breadcrumbs'
-export * from './Button'
-export * from './Card'
-export * from './DNABar'
-export * from './Dialog'
-export * from './Divider'
-export * from './Feedback'
-export * from './Form'
-export * from './Formik'
-export * from './Heading'
-export * from './Hyperlink'
-export * from './ListLink'
-export * from './Modal'
-export * from './Notification'
-export * from './Pagination'
-export * from './PillButton'
-export * from './Table'
-export * from './Tabs'
-export * from './Tag'
-export * from './Text'
-export * from './Toast'
-export * from './Tooltip'
+// ───────────────────────────────────────────────
 
+export {
+    Accordion,
+    AccordionContent,
+    AccordionItem,
+    AccordionTrigger,
+} from './Accordion'
+
+export { BackLink } from './BackLink'
+export type { BackLinkProps } from './BackLink'
+
+export { Badge } from './Badge'
+export type { BadgeProps } from './Badge'
+
+export { Banner } from './Banner'
+export type { BannerProps } from './Banner'
+
+export { BreadCrumbsList, BreadcrumbItem, Breadcrumbs } from './Breadcrumbs'
+export type { BreadcrumbItemProps, BreadcrumbsProps } from './Breadcrumbs'
+
+export { Button } from './Button'
+export type { ButtonProps } from './Button'
+
+export { Card } from './Card'
+export type { CardProps } from './Card'
+
+export { DNABar } from './DNABar'
+export type { DNABarProps } from './DNABar'
+
+export { Dialog } from './Dialog'
+
+export { Divider } from './Divider'
+export type { DividerProps } from './Divider'
+
+export { Feedback } from './Feedback'
+export type { FeedbackProps } from './Feedback'
+
+// ───────────────────────────────────────────────
+// Form Fields
+// ───────────────────────────────────────────────
+
+export { FieldCheckbox } from './Form/FieldCheckbox'
+export type { FieldCheckboxProps } from './Form/FieldCheckbox'
+
+export { FieldCheckboxGroup } from './Form/FieldCheckboxGroup'
+export type { FieldCheckboxGroupProps } from './Form/FieldCheckboxGroup'
+
+export { FieldDate } from './Form/FieldDate'
+export type { FieldDateProps } from './Form/FieldDate'
+
+export { FieldFileUpload } from './Form/FieldFileUpload'
+export type { FieldFileUploadProps, File } from './Form/FieldFileUpload'
+
+export { FieldInput } from './Form/FieldInput'
+export type { FieldInputProps } from './Form/FieldInput'
+
+export { FieldLabel } from './Form/FieldLabel'
+export type { FieldLabelProps } from './Form/FieldLabel'
+
+export { FieldRadio } from './Form/FieldRadio'
+export type { FieldRadioProps } from './Form/FieldRadio'
+
+export { FieldRadioGroup } from './Form/FieldRadioGroup'
+export type { FieldRadioGroupProps } from './Form/FieldRadioGroup'
+
+export { FieldRte, RteMenuButton } from './Form/FieldRte'
+export type {
+    FieldRteProps,
+    TextEditorCustomMenuOptions,
+    TextEditorMenuOptions,
+} from './Form/FieldRte'
+
+export { FieldSelect } from './Form/FieldSelect'
+export type { FieldSelectProps, Option } from './Form/FieldSelect'
+
+export { FieldTextArea } from './Form/FieldTextArea'
+export type { FieldTextAreaProps } from './Form/FieldTextArea'
+
+// ───────────────────────────────────────────────
+// Formik Wrappers
+// ───────────────────────────────────────────────
+
+export { FormikCheckbox } from './Formik/FormikCheckbox'
+export { FormikCheckboxGroup } from './Formik/FormikCheckboxGroup'
+export { FormikDate } from './Formik/FormikDate'
+export { FormikError } from './Formik/FormikError'
+export { FormikFileUpload } from './Formik/FormikFileUpload'
+export { FormikInput } from './Formik/FormikInput'
+export { FormikRadio } from './Formik/FormikRadio'
+export { FormikRadioGroup } from './Formik/FormikRadioGroup'
+export { FormikRte } from './Formik/FormikRte'
+export { FormikSelect } from './Formik/FormikSelect'
+export { FormikTextArea } from './Formik/FormikTextArea'
+
+// ───────────────────────────────────────────────
+// Other Components
+// ───────────────────────────────────────────────
+
+export { Heading } from './Heading'
+export type { HeadingProps } from './Heading'
+
+export { Hyperlink } from './Hyperlink'
+export type { HyperlinkProps } from './Hyperlink'
+
+export { ListLink } from './ListLink'
+export type { ListLinkProps } from './ListLink'
+
+export { Modal } from './Modal'
+export type { ModalProps } from './Modal'
+
+export { Notification } from './Notification'
+export type { NotificationProps } from './Notification'
+
+export { Pagination } from './Pagination'
+export type { PaginationProps } from './Pagination'
+
+export { PillButton } from './PillButton'
+export type { PillButtonProps } from './PillButton'
+
+export { Table } from './Table'
+export type { TableProps } from './Table'
+
+export { TabItem, Tabs } from './Tabs'
+export type { TabsProps } from './Tabs'
+
+export { Tag } from './Tag'
+export type { TagProps } from './Tag'
+
+export { Text } from './Text'
+export type { TextProps } from './Text'
+
+export { ToastContainer, toastNotification } from './Toast'
+export type { ToastContainerProps } from './Toast'
+
+export { Tooltip } from './Tooltip'
+export type { TooltipProps } from './Tooltip'
+
+// ───────────────────────────────────────────────
 // Utils
-export * from './utils'
+// ───────────────────────────────────────────────
 
-// Hooks
-export * from './hooks'
+export { cn, formatBytes, formatDate } from './utils'
 
+// ───────────────────────────────────────────────
 // External
+// ───────────────────────────────────────────────
+
+export { useDrag } from 'react-aria'
 export { FileTrigger } from 'react-aria-components'

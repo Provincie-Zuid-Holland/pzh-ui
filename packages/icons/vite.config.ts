59d8a1baab89b8e1d20f0dddce88007976999f78
Stef Winterswijk <stef_winterswijk@hotmail.com>
2025-08-21 14:06:00 +0200

    Update icons config

--- packages/icons/vite.config.ts
+++ packages/icons/vite.config.ts
@@ -1,40 +1,45 @@
-import typescriptPlugin from "@rollup/plugin-typescript";
-import react from "@vitejs/plugin-react";
-import path from "path";
-import { visualizer } from "rollup-plugin-visualizer";
-import { PluginOption, defineConfig } from "vite";
+import typescriptPlugin from '@rollup/plugin-typescript'
+import react from '@vitejs/plugin-react'
+import path from 'path'
+import { visualizer } from 'rollup-plugin-visualizer'
+import { defineConfig, PluginOption } from 'vite'
 
-const isExternal = (id) => !id.startsWith(".") && !path.isAbsolute(id);
+const isExternal = id => !id.startsWith('.') && !path.isAbsolute(id)
 
 export default defineConfig({
-  plugins: [
-    react({
-      jsxRuntime: "classic",
-      babel: {
-        configFile: true,
-      },
-    }),
-    visualizer(),
-  ],
-  build: {
-    target: "esnext",
-    sourcemap: true,
-    emptyOutDir: true,
-    rollupOptions: {
-      external: isExternal,
-      plugins: [
-        typescriptPlugin({
-          exclude: ["**/*.test.tsx", "**/*.stories.tsx"],
+    plugins: [
+        react({
+            jsxRuntime: 'classic',
+            babel: {
+                configFile: true,
+            },
+        }),
+        visualizer({
+            template: 'treemap', // or sunburst
+            gzipSize: true,
+            brotliSize: true,
+            filename: 'analyse.html', // will be saved in project's root
         }) as PluginOption,
-      ],
-      output: {
-        entryFileNames: "[name].js",
-        preserveModules: true,
-      },
+    ],
+    build: {
+        target: 'esnext',
+        sourcemap: true,
+        emptyOutDir: true,
+        rollupOptions: {
+            external: isExternal,
+            plugins: [
+                typescriptPlugin({
+                    exclude: ['**/*.test.tsx', '**/*.stories.tsx'],
+                }) as PluginOption,
+            ],
+            output: {
+                entryFileNames: '[name].js',
+                preserveModules: true,
+            },
+        },
+        lib: {
+            entry: path.resolve(path.resolve(path.dirname('')), 'src/index.ts'),
+            formats: ['es'],
+        },
     },
-    lib: {
-      entry: path.resolve(path.resolve(path.dirname("")), "src/index.ts"),
-      formats: ["es"],
-    },
-  },
-});
+})

209578f52bad8874d91f7534aa4a60eb9550fb9a
Stef Winterswijk <stef_winterswijk@hotmail.com>
2026-03-10 13:31:21 +0100

    Add optional action to FormikError

--- packages/components/src/Formik/FormikError.tsx
+++ packages/components/src/Formik/FormikError.tsx
@@ -6,17 +6,15 @@ interface FormikErrorProps {
     renderAction?: (message: string) => ReactNode
 }
 
-export const FormikError = ({ name, renderAction }: FormikErrorProps) => {
-    return (
-        <ErrorMessage name={name}>
-            {(message: string) => (
-                <span
-                    className="text-pzh-red-500 text-s mt-1 block"
-                    id={`error-${name}`}>
-                    {message}
-                    {renderAction?.(message)}
-                </span>
-            )}
-        </ErrorMessage>
-    )
-}
+export const FormikError = ({ name, renderAction }: FormikErrorProps) => (
+    <ErrorMessage name={name}>
+        {(message: string) => (
+            <span
+                className="text-pzh-red-500 text-s mt-1 block"
+                id={`error-${name}`}>
+                {message}
+                {renderAction?.(message)}
+            </span>
+        )}
+    </ErrorMessage>
+)
